671243e676cb5c91098ca94a087db8258704d32d
ibessonov <bessonov.ip@gmail.com>
2017-03-14 10:02:25 +0300

    Main examples moved to JUnit tests

---
+++ src/test/java/ibessonov/tailrec/TailrecTest.java
@@ -0,0 +1,124 @@
+package ibessonov.tailrec;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TailrecTest {
+
+    @SuppressWarnings("WeakerAccess")
+    int virtualFactorial(int n, int acc) {
+        if (n <= 1) return acc;
+        return virtualFactorial(n - 1, acc * n);
+    }
+
+    @Test(expected = StackOverflowError.class)
+    public void testVirtualFactorial() {
+        new TailrecTest().virtualFactorial(50_000, 1);
+    }
+
+
+    private static int exceptionalFactorial(int n, int acc) {
+        try {
+            try {
+                if (n <= 1) return acc;
+            } finally {
+                acc *= n;
+            }
+            return exceptionalFactorial(n - 1, acc);
+        } catch (Exception e) {
+            return -1;
+        }
+    }
+
+    @Test(expected = StackOverflowError.class)
+    public void testExceptionalFactorial() {
+        exceptionalFactorial(50_000, 1);
+    }
+
+
+    private static int nonTailRecFactorial(int n) {
+        if (n <= 1) return 1;
+        return n * nonTailRecFactorial(n - 1);
+    }
+
+    @Test(expected = StackOverflowError.class)
+    public void testNonTailRecFactorial() {
+        nonTailRecFactorial(50_000);
+    }
+
+
+    private static int staticFactorial(int n, int acc) {
+        if (n <= 1) return acc;
+        return staticFactorial(n - 1, acc * n);
+    }
+
+    @Test
+    public void testStaticFactorial() {
+        staticFactorial(50_000, 1);
+    }
+
+
+    private int thisFactorial(int n, int acc) {
+        if (n <= 1) return acc;
+        return thisFactorial(n - 1, acc * n);
+    }
+
+    @Test
+    public void testThisFactorial() {
+        new TailrecTest().thisFactorial(50_000, 1);
+    }
+
+
+    private int newThisFactorial(int n, int acc) {
+        if (n <= 1) return acc;
+        return new TailrecTest().newThisFactorial(n - 1, acc * n);
+    }
+
+    @Test
+    public void testNewThisFactorial() {
+        new TailrecTest().newThisFactorial(50_000, 1);
+    }
+
+
+    @SuppressWarnings("WeakerAccess")
+    final int finalFactorial(int n, int acc) {
+        if (n <= 1) return acc;
+        return finalFactorial(n - 1, acc * n);
+    }
+
+    @Test
+    public void testFinalFactorial() {
+        new TailrecTest().finalFactorial(50_000, 1);
+    }
+
+
+    @SuppressWarnings("WeakerAccess")
+    static int add(int x, int y) {
+        if (y == 0) return x;
+        return add(x ^ y, (x & y) << 1);
+    }
+
+    @Test
+    public void testAdd() {
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                if (i + j != add(i, j)) {
+                    assertEquals(i + j, add(i, j));
+                }
+            }
+        }
+    }
+
+
+    @SuppressWarnings("WeakerAccess")
+    static int gcd(int n, int m) {
+        if (m == 0) return n;
+        return gcd(m, n % m);
+    }
+
+    @Test
+    public void testGcd() {
+        assertEquals(3 * 7, gcd(2 * 3 * 5 * 7, 3 * 3 * 7 * 11));
+    }
+}
